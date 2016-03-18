05a5fee41167f9700e5cca0b310367ad8c851cd5
wizzardo <wizzardobox@gmail.com>
2016-03-19 02:17:46 +0300

    prepare for gamepad

--- src/main/java/com/wizzardo/neon/nodes/PlayerControl.java
+++ src/main/java/com/wizzardo/neon/nodes/PlayerControl.java
@@ -1,6 +1,7 @@
 package com.wizzardo.neon.nodes;
 
 import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
@@ -22,6 +23,8 @@ public class PlayerControl extends AbstractControl {
     private Grid grid;
     private long spawnTime;
     public boolean up, down, left, right;
+    public float x;
+    public float y;
 
 
     public PlayerControl(int width, int height, ParticleManager particleManager, Grid grid) {
@@ -43,6 +46,48 @@ public class PlayerControl extends AbstractControl {
 //        move the player in a certain direction
 //        if he is not out of the screen
         PlayerNode player = this.player;
+        if (x != 0 || y != 0) {
+            float angle = FastMath.PI / 2 - FastMath.atan2(x, y);
+            player.getLocalRotation().fromAngleNormalAxis(angle, Vector3f.UNIT_Z);
+            particleManager.makeExhaustFire(spatial.getLocalTranslation(), angle, spawnTime);
+            grid.applyDirectedForce(new Vector3f(0, 0, 5000), player.getLocalTranslation(), 100);
+        }
+
+        float joystickBoost = 30;
+        if (x != 0) {
+            if (x > 0) {
+                if (player.getLocalTranslation().x < screenWidth - player.getRadius()) {
+                    player.move(tpf * speed * x * joystickBoost, 0, 0);
+                    x -= 0.001;
+                    if (x < 0)
+                        x = 0;
+                }
+            } else {
+                if (player.getLocalTranslation().x > player.getRadius()) {
+                    player.move(tpf * speed * x * joystickBoost, 0, 0);
+                    x += 0.001;
+                    if (x > 0)
+                        x = 0;
+                }
+            }
+        }
+        if (y != 0) {
+            if (y > 0) {
+                if (player.getLocalTranslation().y < screenHeight - player.getRadius()) {
+                    player.move(0, tpf * speed * y * joystickBoost, 0);
+                    y -= 0.001;
+                    if (y < 0)
+                        y = 0;
+                }
+            } else {
+                if (player.getLocalTranslation().y > player.getRadius()) {
+                    player.move(0, tpf * speed * y * joystickBoost, 0);
+                    y += 0.001;
+                    if (y > 0)
+                        y = 0;
+                }
+            }
+        }
         if (up) {
             if (player.getLocalTranslation().y < screenHeight - player.getRadius()) {
                 player.move(0, tpf * speed, 0);
